1710d47a7e50741c9a297827338c3a53a511b87a
Lin Wang <leihn.wong@gmail.com>
2016-11-26 19:58:37 +0100

    Add greedy

--- model/model.py
+++ model/model.py
@@ -92,6 +92,7 @@ def create_abs_opt():
 
 
 def create_abs_approx():
+
     model = AbstractModel()
 
     # Parameters cloud, and user
@@ -130,13 +131,13 @@ def create_abs_approx():
         # Reconfiguration cost
         cost_rc = 0
         for i in model.I:
-            cur = sum(model.x[i, j] for j in model.J)
-            pre = sum(model.pre[i, j] for j in model.J)
+            cur_load = sum(model.x[i, j] for j in model.J)
+            pre_load = sum(model.pre[i, j] for j in model.J)
             cost_rc += model.c[i] / log(1 + model.cap[i] / model.eps1) * \
                        (
-                           (cur + model.eps1) *
-                           log((cur + model.eps1) / (pre + model.eps1)) -
-                           cur
+                           (cur_load + model.eps1) *
+                           log((cur_load + model.eps1) / (pre_load + model.eps1)) -
+                           cur_load
                        )
 
         # Delay penalty cost
@@ -152,6 +153,78 @@ def create_abs_approx():
                                log((model.x[i, j] + model.eps2) / (model.pre[i, j] + model.eps2)) -
                                model.x[i, j]
                            )
+
+        # return cost_op + cost_rc + cost_dl + cost_mg
+        return (cost_op + cost_dl) * model.alpha + (cost_rc + cost_mg) * model.beta
+
+    model.OBJ = Objective(rule=obj_expr)
+
+    def cov_const_rule(model, j):
+        return sum(model.x[i, j] for i in model.I) == model.lbd[j]
+
+    def cap_const_rule(model, i):
+        return sum(model.x[i, j] for j in model.J) <= model.cap[i]
+
+    # Constraints
+    model.CovConst = Constraint(model.J, rule=cov_const_rule)
+    model.CapConst = Constraint(model.I, rule=cap_const_rule)
+
+    return model
+
+
+def create_abs_greedy():
+    model = AbstractModel()
+
+    # Parameters cloud, and user
+    model.i = Param(within=NonNegativeIntegers)
+    model.j = Param(within=NonNegativeIntegers)
+
+    # Index sets
+    model.I = RangeSet(1, model.i)
+    model.J = RangeSet(1, model.j)
+
+    # Coefficients and right-hand-side data
+    model.a = Param(model.I)  # Operational price
+    model.c = Param(model.I)  # Reconfiguration price
+    model.d = Param(model.I, model.J)  # Delay price
+    model.b = Param(model.I)  # Migration price
+
+    # Regularization paramters
+    model.eps1 = Param(within=NonNegativeReals)
+    model.eps2 = Param(within=NonNegativeReals)
+    model.pre = Param(model.I, model.J)
+
+    model.lbd = Param(model.J)  # User workload
+    model.cap = Param(model.I)  # Cloud capacity
+
+    # Variable: workload from user j on cloud i at time t
+    model.x = Var(model.I, model.J, domain=NonNegativeReals, initialize=0)
+
+    # Objective expression
+    def obj_expr(model):
+
+        # Operational cost
+        cost_op = sum(sum(
+            model.a[i] * model.x[i, j]
+            for j in model.J) for i in model.I)
+
+        # Reconfiguration cost
+        cost_rc = 0
+        for i in model.I:
+            cur_load = sum(model.x[i, j] for j in model.J)
+            pre_load = sum(model.pre[i, j] for j in model.J)
+            inc_load = cur_load - pre_load
+            cost_rc += model.c[i] * (abs(inc_load) + inc_load) / 2
+
+        # Delay penalty cost
+        cost_dl = summation(model.d, model.x)
+
+        # Migration cost
+        cost_mg = 0
+        for i in model.I:
+            for j in model.J:
+                mg_load = model.x[i, j] - model.pre[i, j]
+                cost_mg += model.b[i] * (abs(mg_load) + mg_load) / 2
 
         # return cost_op + cost_rc + cost_dl + cost_mg
         return (cost_op + cost_dl) * model.alpha + (cost_rc + cost_mg) * model.beta
